d6c007514d36e27c8983a0cf28e5f062efef7bfb
leidsten <linn@getacademy.no>
2022-08-10 13:01:57 +0200

    x

---
+++ letterCounter.js
@@ -0,0 +1,26 @@
+function count(text, countLetter) {
+    text = text.toLowerCase();
+    let count = 0;
+    if(checkIfLetter(countLetter)){
+        for (let letter of text) {
+            if (letter == countLetter) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+    else return "Can only count letters. Try again.";
+}
+
+function checkIfLetter(character){
+  // return character.toUpperCase() != character.toLowerCase()   -- ditch dette, sjekk ascii
+    return character >= 'a' &&  character <= 'z' || character == 'æ' || character == 'ø' || character == 'å';
+
+}
+
+
+// Hva kan gå galt?
+//stor bokstav -V, tall-V, symbol -V, space -V
+
+//
