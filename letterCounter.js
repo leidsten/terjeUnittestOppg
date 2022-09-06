cc822c1c659f042466372d1e60322845c3a088f1
leidsten <linn@getacademy.no>
2022-09-06 10:27:04 +0200

    added check for null, undefined, empty searchtext and no such letter in searchtext

--- letterCounter.js
+++ letterCounter.js
@@ -1,41 +1,23 @@
-let charCount = 0;
 function count(text, countLetter) {
-    charCount = 0;
-    if(!checkIfNullOrUndefined(countLetter)) return null;
-
+    let charCount = 0;
+   
+    if (!checkIfNullOrUndefined(countLetter))return null;
+    
     text = text.toLowerCase();
     countLetter = countLetter.toLowerCase();
-
-    if (checkIfLetter(countLetter)) {
-        searchForCharacter(countLetter, text);
-    }
-    else {
-        searchForCharacter(countLetter, text);
-    }
-
-    return charCount;
-
-}
-
-function checkIfNullOrUndefined(countLetter){
-    if(countLetter != null || countLetter != undefined)return true;
-    return false;
-}
-
-function searchForCharacter(characterToFind, text){
     for (let character of text) {
-        if (character == characterToFind) {
+        if (character == countLetter) {
             charCount++;
         }
     }
+    return charCount;
 }
 
-
-function checkIfLetter(character) {
-    return character >= 'a' && character <= 'z' ||
-        character >= 'A' && character <= 'Z' ||
-        character == 'æ' || character == 'Æ' ||
-        character == 'ø' || character == 'Ø' ||
-        character == 'å' || character == 'Å';
-
+function checkIfNullOrUndefined(countLetter) {
+    if (countLetter != null || countLetter != undefined)return true; 
+    return false; 
 }
+
+// function checkIfNullOrUndefined(countLetter) {
+//     return countLetter === null && countLetter === undefined;
+// }
